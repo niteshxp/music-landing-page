0f08de0c38a9381191a68a16e1c195e8bc13d9f8
niteshxp <niteshpatel8852@gmail.com>
2024-03-08 12:45:01 +0530

    add images

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from "react";
 import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
 
+
 const musicSchoolTestimonials = [
   {
     quote:
